a614595d3cee61e2cb1b3f6307bb5f743bb70d6a
VanshikaKarun <vanshikazimone999@gmail.com>
2025-07-23 22:51:35 +0530

    Adding Frontend

---
+++ frontend/src/components/RequestLetter.js
@@ -0,0 +1,50 @@
+// create a form and store the student details
+
+import { useState } from "react";
+import { toBigInt } from "ethers";
+
+function RequestLetter({ state }) {
+    const [name, setName] = useState("");
+    const [university, setUniversity] = useState("");
+    const [program, setProgram] = useState("");
+    const [id, setId] = useState("");
+    const { contract } = state;
+
+    // same as asyn function handleRequest(){}
+    const handleRequest = async () => {
+        try {
+            if (!contract) {
+                alert("Smart contract not loaded");
+                return;
+            }
+            const txnResp = await contract.requestRecommendation(name, university, program);
+            await txnResp.wait();
+
+            let cnt = await contract.studentCount();
+            cnt = toBigInt(cnt) - 1n;
+            setId(cnt.toString());
+        }
+        catch (error) {
+            console.error(error);
+            alert("Error requesting Recommendation");
+        }
+    }
+
+    return (
+        <div>
+            <div>
+                <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
+                <input type="text" placeholder="University" value={university} onChange={(e) => setUniversity(e.target.value)} />
+                <input type="text" placeholder="Program" value={program} onChange={(e) => setProgram(e.target.value)} />
+            </div>
+
+            <div>
+                <button onClick={handleRequest}>Request Recommendation</button>
+            </div>
+
+            {id !== "" && (<div> Recommendation id: {id}</div>)}
+        </div>
+    )
+}
+
+export default RequestLetter;
